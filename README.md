6d556f2529a21544f653fdb3633bc09a9aa5f8cb
Emanuel Garcés <sagazu13@gmail.com>
2021-05-18 22:06:47 -0500

    Readme agregado

---
+++ README.md
@@ -0,0 +1,12 @@
+# Notas 
+Este es mi primer programa de Nodejs
+
+```
+Options:
+      --help     Show help                                             [boolean]
+      --version  Show version number                                   [boolean]
+  -b, --base     Es la base de la tabloa de multiplicar      [number] [required]
+  -h, --hasta    Este es el numero hasta donde quieres crear la tabla
+                                               [number] [required] [default: 10]
+  -l, --listar   Muestra la tabla en consola          [boolean] [default: false]
+```
--- config/yargs.js
+++ config/yargs.js
@@ -2,19 +2,23 @@ const yargs = require('yargs')
         .option('b', {
             alias: 'base',
             type: 'number',
+            describe: 'Es la base de la tabloa de multiplicar',
             demandOption: true,
-        })
-        .option('l', {
-            alias: 'listar',
-            type: 'boolean',
-            default: false
         })
         .option('h', {
             alias: 'hasta',
             type: 'number',
             demandOption: true,
-            describe: 'Hasta que tabla quiere crear'
+            default: 10,
+            describe: 'Este es el numero hasta donde quieres crear la tabla'
         })
+        .option('l', {
+            alias: 'listar',
+            type: 'boolean',
+            describe: 'Muestra la tabla en consola',
+            default: false
+        })
+
         .check( (argv) => {
             if( isNaN(argv.b) ) {
                 throw 'LA BASE TIENE QUE SER UN NUMERO';
